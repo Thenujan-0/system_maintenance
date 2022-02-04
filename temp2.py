655cd725f2e40b0804b19ccaa5f154f2fc8d909e
Thenujan <thenujan006@gmail.com>
2022-02-04 19:09:01 +0530

    made a dialog box to show when selected partition for swap is too big created a validator for swappiness .made swappiness almost functional

--- temp2.py
+++ temp2.py
@@ -1,53 +1,20 @@
 import subprocess
 
-with open ('out.txt','r') as f:
-    a =f.read()
-lines= a.splitlines()
-target_lines=[]
+
+
+# assumes that
+out= subprocess.check_output(['df -Th'],shell=True).decode()
+
+print(out)
+lines =out.splitlines()
+
+#store the indexes of the lines that are suitable for swap
+target_line_indexes=[]
+
+
 for line in lines:
-    if line[:7] =='/dev/sd':
-        print(line)
-        target_lines.append(line)
-        
-class Device():
-    name='/dev/sdX'
-    partitions=[]
-    
-    def __init__(self,name,partitions):
-        self.name = name
-        self.partitions=partitions
-        
-    def addPartition(self,partition):
-        self.partitions.append(partition)
-    
-
-class Partition():
-    name:str
-    size:str
-    
-    def __init__(self,name,size):
-        self.name=name
-        self.size=size
-
-device_list=[]
-
-for line in target_lines:
     words=line.split()
-    if len(device_list)==0:
-        if '*' ==words[1]:
-            device_list.append(Device(words[0][0:8],[Partition(words[0],words[5])]))
-        else:
-            device_list.append(Device(words[0][0:8],[Partition(words[0],words[4])]))
-    else:
-        if device_list[-1].name==words[0][0:8]:
-            if '*' ==words[1]:
-                device_list[-1].addPartition(Partition(words[0][0:9],words[5]))
-            else:
-                device_list[-1].addPartition(Partition(words[0][0:9],words[4]))
-        else:
-            device_list.append(Device(words[0],[words[0]]))
-        
-        
-        
-        
-        
+
+    if '/dev/' in words[0]:
+        target_line_indexes.append(lines.index(line))
+
